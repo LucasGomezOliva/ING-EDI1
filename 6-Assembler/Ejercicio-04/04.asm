3309a2fc4276f3a21045ebdf1777675ab6489c73
LucasGomezOliva <lucasgomezoliva@gmail.com>
2022-10-25 16:17:02 -0300

    Fix

--- 6-Assembler/Ejercicio-04/04.asm
+++ 6-Assembler/Ejercicio-04/04.asm
@@ -1,9 +1,19 @@
-JMP inicio
+;------------------------------------------------------------------------------
 
-inicio:
-	mov b,0x01
-loop:
-	shl b,1
-	cmp b,0x80
-	je inicio
-	jmp loop
+;Ejercicio-04
+
+;Implemente una rutina que cargue en un array los códigos de un contador anillo de módulo 8 utilizando las instrucciones de desplazamiento:
+
+;00000001-00000010-000001000...hasta el 10000000
+
+;------------------------------------------------------------------------------
+
+JMP INICIO
+
+INICIO:
+	MOV 	B,0x01
+LOOP:
+	SHL 	B,1
+	CMP 	B,0x80
+	JE		INICIO
+	JMP 	LOOP
